2f4592b483dc5bc61ae3df3622e7ead715ab52ae
Danny Qiu <dqiu55@gmail.com>
2019-04-16 00:37:16 -0400

    Use numpy vector inner product instead of for-loop on spacy vector similarity

--- app/irsystem/models/thesaurus.py
+++ app/irsystem/models/thesaurus.py
@@ -1,13 +1,17 @@
+import numpy as np
 import spacy
+from collections import defaultdict
 
 class Thesaurus:
 
     def __init__(self):
         self.nlp = spacy.load('en_core_web_lg')
         self.vocab = [w for w in self.nlp.vocab if w.is_lower and w.prob >= -15 and w.vector.any()]
+        self.vocab_vectors = np.array([w.vector for w in self.vocab])
 
     def most_similar(self, word, top=10):
-        word = self.nlp.vocab[word]
-        by_similarity = sorted(self.vocab, key=lambda w: word.similarity(w), reverse=True)
+        word_vector = self.nlp.vocab[word].vector
+        sim = np.inner(word_vector, self.vocab_vectors)
+        by_similarity = [self.vocab[i] for i in np.argsort(-sim)]
         top_similar = by_similarity[:top]
         return [w.lower_ for w in top_similar]
