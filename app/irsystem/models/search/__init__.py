5e912fbe0f30d8c9acd3d6674e929d3b527eeed1
Danny Qiu <dqiu55@gmail.com>
2019-04-22 12:10:21 -0400

    Split search models into separate files

---
+++ app/irsystem/models/search/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from app import app
+
+DATA_DIR = os.path.abspath(os.path.join(app.instance_path, "..", "data"))
+FUN_FACT_TITLE_CSV = os.path.abspath(os.path.join(DATA_DIR, "fun_fact_title.csv"))
+TIL_TITLE_CSV = os.path.abspath(os.path.join(DATA_DIR, "til_title.csv"))
+YSK_TITLE_CSV = os.path.abspath(os.path.join(DATA_DIR, "ysk_title.csv"))
+
+REQUIRED_COLUMNS = [
+    "title",
+    "subreddit",
+    "permalink",
+]
+
+# Bring search models to package level
+from .boolean import BooleanSearch
+from .dummy import DummySearch
