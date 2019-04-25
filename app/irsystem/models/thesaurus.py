606f4f161a7d7d15b41e254cffc8bb14401a4eec
Danny Qiu <dqiu55@gmail.com>
2019-04-24 23:34:15 -0400

    Optimize memory usage for thesaurus by pickling spacy vectors instead of loading en_core_web_lg

--- app/irsystem/models/thesaurus.py
+++ app/irsystem/models/thesaurus.py
@@ -1,20 +1,39 @@
+import os
 import numpy as np
+import pickle
 import spacy
 from collections import defaultdict
+
+from app import app
+
+PICKLE_DIR = os.path.abspath(os.path.join(app.instance_path, "..", "pkl"))
 
 class Thesaurus:
 
     def __init__(self):
-        self.nlp = spacy.load('en_core_web_lg')
-        self.vocab = [w for w in self.nlp.vocab if w.is_lower and w.prob >= -15 and w.vector.any()]
-        self.vocab_vectors = np.array([w.vector for w in self.vocab])
+        try:
+            with open(os.path.join(PICKLE_DIR, "thesaurus.vocab.pkl"), 'rb') as f:
+                self.vocab = pickle.load(f)
+            with open(os.path.join(PICKLE_DIR, "thesaurus.vocab_vectors.pkl"), 'rb') as f:
+                self.vocab_vectors = pickle.load(f)
+        except Exception as e:
+            print("No pickle file, recomputing...", e)
+            nlp = spacy.load('en_core_web_lg')
+            nlp_vocab = [w for w in nlp.vocab if w.is_lower and w.prob >= -15 and w.vector.any()]
+            self.vocab = [w.lower_ for w in nlp_vocab]
+            self.vocab_vectors = np.array([w.vector for w in nlp_vocab])
+            with open(os.path.join(PICKLE_DIR, "thesaurus.vocab.pkl"), 'wb') as f:
+                pickle.dump(self.vocab, f)
+            with open(os.path.join(PICKLE_DIR, "thesaurus.vocab_vectors.pkl"), 'wb') as f:
+                pickle.dump(self.vocab_vectors, f)
+        self.vocab_index = {w: i for i, w in enumerate(self.vocab)}
 
     def most_similar(self, word, top=10):
-        word_vector = self.nlp.vocab[word].vector
         # word does not appear in our vocabulary, doesn't make sense to give any similar words
-        if not np.any(word_vector):
+        if word not in self.vocab_index:
             return [word]
-        sim = np.inner(word_vector, self.vocab_vectors)
+        word_vector = self.vocab_vectors[self.vocab_index[word]]
+        sim = self.vocab_vectors.dot(word_vector)
         by_similarity = [self.vocab[i] for i in np.argsort(-sim)]
         top_similar = by_similarity[:top]
-        return [w.lower_ for w in top_similar]
+        return top_similar
